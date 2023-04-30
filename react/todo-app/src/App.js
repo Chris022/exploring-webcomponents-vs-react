7d095c2eb8adc2c9923750bfc93d043e1ea82069
Christoph <christoph@debian>
2023-04-30 16:09:24 +0200

    alert once checkbox is checked

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -1,22 +1,42 @@
-import Heading from './components/heading/Heading';
+import React from "react";
 
+import Heading from './components/heading/Heading';
 import Card from './components/card/Card';
 import Button from './components/button/Button';
-
 import Todo from './components/todo/Todo';
 import Spacer from './components/spacer/Spacer';
 
 import './App.css';
 
-
 function App() {
+
+  let [todos,setTodos] = React.useState([]);
+
+  let add_todo = ()=>{
+    let name = window.prompt("Todo:","");
+    if(name==="")return;
+    let todo = {
+      text:name,
+      done:false
+    }
+    setTodos([...todos,todo]);
+  }
+
+  let check_todo = (i) =>{
+    let todos_buff =  [...todos]
+    todos_buff[i].done = !todos_buff[i].done;
+    setTodos(todos_buff)
+  }
+
   return (
     <div className='app'>
       <Heading text="ToDo - React"></Heading>
       <Card>
-        <Todo text="I am a Text way to long to fit into a single line. bla bla bla bla" done={false}></Todo>
+        {todos.map((todo,i) => (
+          <Todo key={"todo-"+i} text={todo.text} done={todo.done} onCheck={()=>check_todo(i)}></Todo>
+        ))}
         <Spacer />
-        <Button text="Add ToDo"></Button>
+        <Button text="Add ToDo" onClick={add_todo}></Button>
       </Card>
     </div>
   );
--- react/todo-app/src/components/todo/Todo.js
+++ react/todo-app/src/components/todo/Todo.js
@@ -4,6 +4,10 @@ import React from "react";
 import "./Todo.css"
 
 export default function Todo({text,done,onCheck}){
+    React.useEffect(()=>{
+        if(done) alert("ToDo erledigt!");
+    },[done])
+
     return (
         <div className="todo">
             <input type="checkbox" checked={done} onChange={onCheck}/>
